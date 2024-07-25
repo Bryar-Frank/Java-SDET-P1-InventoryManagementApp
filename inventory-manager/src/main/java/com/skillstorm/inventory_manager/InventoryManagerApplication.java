5c11e477e8bccb56e285917240ceef841699c257
Bryar Frank <13ryarfrank@gmail.com>
2024-07-25 17:38:32 -0400

    adds postgres db connection

--- inventory-manager/src/main/java/com/skillstorm/inventory_manager/InventoryManagerApplication.java
+++ inventory-manager/src/main/java/com/skillstorm/inventory_manager/InventoryManagerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventoryManagerApplication {
 
 	public static void main(String[] args) {
-		run(InventoryManagerApplication.class, args);
+		SpringApplication.run(InventoryManagerApplication.class, args);
 	}
 
 }
