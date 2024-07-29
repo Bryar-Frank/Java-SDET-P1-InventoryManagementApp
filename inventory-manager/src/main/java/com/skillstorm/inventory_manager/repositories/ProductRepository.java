2ef6c6fb3b81b1e4c0228d633c0d0aacbcc2b5ea
Bryar Frank <13ryarfrank@gmail.com>
2024-07-29 18:39:34 -0400

    adds repos for product and warehouse for use in InventoryService

---
+++ inventory-manager/src/main/java/com/skillstorm/inventory_manager/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.skillstorm.inventory_manager.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.skillstorm.inventory_manager.models.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    
+}
