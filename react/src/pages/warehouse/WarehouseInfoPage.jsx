b75364a4bc9de8a32fee4a07e2dbc430244f01b5
Bryar Frank <13ryarfrank@gmail.com>
2024-08-01 21:44:36 -0400

    adds GET method for warehouses and warehouse info page

--- react/src/pages/warehouse/WarehouseInfoPage.jsx
+++ react/src/pages/warehouse/WarehouseInfoPage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom"
+import WarehouseTable from "../../components/tables/WarehouseTable"
 
 export function WarehouseInfoPage () {
     
     
     return <>
         <h1>TODO: Show Warehouse Info</h1>
+        <WarehouseTable />
         <Link to="/newwarehouse"><button id="addwarehouse" >Add New Warehouse</button></Link>
     </>
 }
